0933c47db1b8b49ec8c78ee24f5237e1eaa26317
JongMany <blackberry1114@naver.com>
2024-03-05 12:01:37 +0900

    [ :sparkles: || Feat ] redirect, rewrite 기능 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,21 @@ import { auth } from "@/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  // console.log("middleware", request);
+  // console.log("middleware", request)
+  const { pathname } = request.nextUrl;
   const session = await auth();
-  // console.log("middleware", session);
-  if (!session) {
+  console.log("middleware", session);
+  if (session && pathname === "/") {
+    return NextResponse.rewrite("http://localhost:3000/main");
+  } else if (!session && pathname !== "/") {
     // console.log("redirect!");
     return NextResponse.redirect("http://localhost:3000/");
+  } else {
+    console.log("nothing");
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/main"],
+  matcher: ["/main", "/"],
 };
