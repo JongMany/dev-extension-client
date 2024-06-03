0adb389b181b730f9e8a228961c956e8474f8852
JongMany <blackberry1114@naver.com>
2024-06-04 01:17:10 +0900

    feature: 메인화면 UI 구현

--- src/app/(main)/main/_components/HeadTitle.tsx
+++ src/app/(main)/main/_components/HeadTitle.tsx
@@ -1,13 +1,25 @@
+import Image from "next/image";
+
 export default function HeadTitle() {
   return (
-    <section className="flex flex-col items-center pt-10 pb-20 border-b-2 ">
+    <section className="flex flex-col items-center pt-10 pb-20 border-b-2 h-[90vh]">
       <h1 className="py-6 font-bold text-2xl flex justify-center">
         Study Log | 당신의 개발 시간을 관리해드립니다.
       </h1>
-      <p className="flex flex-col items-center text-center font-bold text-lg mt-8">
+      <p className="flex flex-col items-center text-center font-bold text-lg mt-8 mb-6">
         The Extension for Developers. <br />
-        Collect and organize your study time.
+        Collect and organize your development time.
       </p>
+      <div className="flex-1 w-[80%] pb-4">
+        <div className="relative h-full">
+          <Image
+            src={"/assets/image/main_image_0.webp"}
+            alt={"소개 이미지"}
+            fill
+            className="object-fill rounded-xl"
+          />
+        </div>
+      </div>
     </section>
   );
 }
