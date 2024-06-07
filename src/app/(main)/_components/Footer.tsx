c260f2de4d0f26a40707333ecd2fdd4a12835cf0
JongMany <blackberry1114@naver.com>
2024-06-08 04:02:01 +0900

    style: dashboard UI 배치

--- src/app/(main)/_components/Footer.tsx
+++ src/app/(main)/_components/Footer.tsx
@@ -16,28 +16,32 @@ export default async function Footer() {
         <h4 className="text-lg font-semibold mb-2">
           저희의 익스텐션을 통해 <br /> 코딩을 효율적으로 해보세요
         </h4>
-        <div className="flex flex-col text-[14px] font-semibold ">
-          <span>
-            <Link href="/main">홈</Link>
-          </span>
-          <span>
-            <Link href="/dashboard">대시보드</Link>
-          </span>
-          <span>
-            <Link href="/goal">목표</Link>
-          </span>
-          <span>
-            <Link href="/rank">랭킹</Link>
-          </span>
-          <span>
-            <Link
-              href={`/profile/${
-                session?.user.email || "blackberry1114@naver.com"
-              }`}
-            >
-              프로필
-            </Link>
-          </span>
+        <div className="mt-2 flex gap-x-5 text-[14px] font-semibold ">
+          <div className="flex flex-col">
+            <span>
+              <Link href="/main">홈</Link>
+            </span>
+            <span>
+              <Link href="/dashboard">대시보드</Link>
+            </span>
+          </div>
+          <div className="flex flex-col">
+            <span>
+              <Link href="/goal">목표</Link>
+            </span>
+            <span>
+              <Link href="/rank">랭킹</Link>
+            </span>
+            <span>
+              <Link
+                href={`/profile/${
+                  session?.user.email || "blackberry1114@naver.com"
+                }`}
+              >
+                프로필
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
       <div className="flex-1 flex flex-col items-center">
